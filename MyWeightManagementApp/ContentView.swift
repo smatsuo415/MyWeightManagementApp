31304d647e1d269917e296093e4655199d3d1e4e
smatsuo415 <shu55555.m@gmail.com>
2021-05-08 21:20:40 +0900

    Update weightManagementView : Create List View and Graph View

--- MyWeightManagementApp/ContentView.swift
+++ MyWeightManagementApp/ContentView.swift
@@ -28,9 +28,12 @@ struct UserListsView: View, Identifiable{
 }
 
 struct WeightManagementView: View {
+    @Environment(\.managedObjectContext) private var viewContext
+    
     let user:UserListsView
     @State var val: Date = Date()
     @State var sWeight: String = ""
+    @State var graphFlag: Bool = true
 
     struct UserData :Identifiable{
         var id: Int
@@ -40,6 +43,23 @@ struct WeightManagementView: View {
     }
     @State var userDataList:[UserData] = []
     
+    /// データ取得処理
+    @FetchRequest(
+        entity: Item.entity(),
+        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timeStamp, ascending: true)],
+            predicate: nil
+    ) private var items: FetchedResults<Item>
+    
+    // 日付情報の文字列変換
+    var dateFormat: DateFormatter {
+    
+        let dformat = DateFormatter()
+        dformat.dateStyle = .long
+        dformat.timeStyle = .none
+        dformat.dateFormat = "yyyy/MM/dd"
+        return dformat
+    }
+    
     var body: some View {
         VStack{
             Text(user.name)
@@ -48,17 +68,47 @@ struct WeightManagementView: View {
             Spacer(minLength: 10)
             
             Form{
-                List(userDataList) {userData in
-                    HStack{
-                        Text(userData.name)
-                            .font(.title)
-                        Text(":")
-                            .font(.title)
-                        Text(String(userData.weight))
-                            .font(.title)
+                if( !graphFlag ){
+                    List{
+                        ForEach(items) { item in
+                            if( item.name! == user.name ){
+                                Text("\(item.name!) : \(dateFormat.string(from: item.timeStamp!)) : \(String(item.weight))")
+                                    .font(.title)
+                            }
+                        }
+                        .onDelete(perform: deleteItem)
                     }
                 }
+                else {
+                    Text("グラフ表示")
+                }
             }
+
+            Divider()
+            
+            Toggle(isOn: $graphFlag, label: {
+                Text("グラフ表示")
+            })
+            .padding(10)
+            
+            // デバッグ用 items全削除ボタン
+//            Button(action: {
+//                for item in items {
+//                    viewContext.delete(item)
+//                }
+//
+//                try? viewContext.save()
+//            },
+//                label: {
+//                    Text("消去")
+//                        .font(.largeTitle)
+//                        .background(Color.blue)
+//                        .foregroundColor(Color.white)
+//                        .padding(5)
+//                }
+//            )
+                
+            Divider()
             
             HStack{
                 DatePicker("", selection: $val, displayedComponents:.date)
@@ -66,23 +116,25 @@ struct WeightManagementView: View {
                 Spacer(minLength: 10)
                 
                 Text("体重：")
-                TextField("weight", text: $sWeight)
+                TextField("weight", text: Binding(
+                            get: {sWeight},
+                            set: {sWeight = $0.filter{"0123456789".contains($0)}}))
                     .padding(10)
                     .textFieldStyle(RoundedBorderTextFieldStyle())
-                    .keyboardType(.decimalPad)
                 
                 Spacer(minLength: 10)
                 
                 Button(action: {
                     print("[btn]sWeight = \(sWeight)")
-                    let data = UserData(id: userDataList.count + 1,
-                                        name: user.name,
-                                        date: val,
-                                        weight: sWeight
-                                        )
-                    userDataList.append(data)
 
-                }, label:{
+                    /// 体重新規登録処理
+                    let newItem = Item(context: viewContext)
+                    newItem.name = user.name
+                    newItem.weight = NSString(string: sWeight).floatValue
+                    newItem.timeStamp = val
+                    
+                    try? viewContext.save()
+                }, label: {
                     Text("登録")
                         .padding(8)
                         .background(Color.blue)
@@ -91,6 +143,19 @@ struct WeightManagementView: View {
                 })
             }
         }
+    }
+    
+    /// 体重情報の削除
+    /// - Parameter offsets: 要素番号のコレクション
+    func deleteItem(offsets: IndexSet) {
+        
+        for index in offsets {
+            
+            print("[Del] index: \(index)")
+            viewContext.delete(items[index])
+        }
+        
+        try? viewContext.save()
     }
 }
 
@@ -103,6 +168,12 @@ struct ContentView: View {
         sortDescriptors: [NSSortDescriptor(keyPath: \User.nid, ascending: true)],
             predicate: nil
     ) private var users: FetchedResults<User>
+    
+    @FetchRequest(
+        entity: Item.entity(),
+        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timeStamp, ascending: true)],
+            predicate: nil
+    ) private var items: FetchedResults<Item>
     
     @State var userName:String = ""
     
@@ -177,6 +248,14 @@ struct ContentView: View {
             print("[Del] index: \(index)")
             print("[Del] users.count prev : \(users.count)")
             
+            // 削除ユーザーの体重情報削除
+            for item in items {
+                if( users[index].name == item.name) {
+                    viewContext.delete(item)
+                }
+            }
+            
+            // 残りユーザーのID繰上げ
             if( index + 1 < users.count ){
                 for index2 in index + 1...users.count - 1 {
                     print("[Del] index2: \(index2)")
